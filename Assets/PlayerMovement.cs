f480b437ca31dd7be264299616ed28b59044440d
garganjohn <garganjohn@gmail.com>
2019-09-02 18:11:45 -0400

    added left/right movement and jumping

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour{
+
+    public CharacterController2D controller;
+
+    public float runSpeed = 100f;
+    float horizontalMove = 0f;
+    bool jump = false;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Debug.Log(Input.GetAxisRaw("Horizontal"));
+        horizontalMove = Input.GetAxisRaw("Horizontal")  * runSpeed;
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        // Move our character
+        //Three params, movement speed, ifCrouching, ifJumping
+        controller.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
+        jump = false;
+    }
+}
