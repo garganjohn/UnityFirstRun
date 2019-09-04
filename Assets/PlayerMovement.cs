03df22cdc7a38377688133bbf5e3cf1beb187835
garganjohn <garganjohn@gmail.com>
2019-09-03 22:42:16 -0400

    updated unity to latest version, added animations for Player Sprite

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -1,46 +1,42 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour{
+public class PlayerMovement : MonoBehaviour {
 
     public CharacterController2D controller;
-
-    public float runSpeed = 20000f;
+    public Animator animator;
+    public float runSpeed = 200f;
     float horizontalMove = 0f;
     bool jump = false;
     bool crouch = false;
 
 
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    void Start(){
+
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        //Debug.Log(Input.GetAxisRaw("Horizontal"));
-        horizontalMove = Input.GetAxisRaw("Horizontal")  * runSpeed;
+    void Update() { 
+    //Debug.Log(Input.GetAxisRaw("Horizontal"));
+    horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
 
-        if (Input.GetButtonDown("Jump"))
-        {
+        if (Input.GetButtonDown("Jump")){
             jump = true;
         }
 
-        if (Input.GetButtonDown("Crouch"))
-        {
-        crouch = true;
+        if (Input.GetButtonDown("Crouch")){
+            crouch = true;
 
-        }else if (Input.GetButtonUp("Crouch"))
-        {
+        }
+        else if (Input.GetButtonUp("Crouch")){
             crouch = false;
         }
     }
 
-    private void FixedUpdate()
-    {
+    private void FixedUpdate(){
         // Move our character
         //Three params, movement speed, ifCrouching, ifJumping
         controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
